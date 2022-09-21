acaac7861d5303c352aecc8f984eb559b2280c4a
ilahyani <lahyani799@gmail.com>
2022-09-21 22:04:00 +0100

    RandomChump constractor update

--- module01/BraiiiiiiinnnzzzZ/randomChump.cpp
+++ module01/BraiiiiiiinnnzzzZ/randomChump.cpp
@@ -6,13 +6,15 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 16:19:51 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/09/20 12:04:38 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/09/21 22:00:39 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void    randomChump(std::string name) {
-    Zombie  zombie(name);
+    Zombie  zombie;
+
+    zombie.setName(name);
     zombie.announce();
 }
