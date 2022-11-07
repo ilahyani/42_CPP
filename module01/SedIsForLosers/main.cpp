b75a90f9ba05a11fea87699a5ecedbe162dae650
ilahyani <lahyani799@gmail.com>
2022-11-07 15:08:39 +0100

    ex04 done

--- module01/SedIsForLosers/main.cpp
+++ module01/SedIsForLosers/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:14:27 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/11/03 15:23:57 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:06:31 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,33 @@
 
 int main(int ac, char **av)
 {
-    std::ofstream   ost(av[1]);
-    std::ifstream   ifst(av[1]);
+    std::ofstream   os;
+    std::ifstream   ifs;
+    std::string     data[3];
     std::string     str;
+    size_t          pos;
 
-    // if (ac != 4) {
-    //     std::cout << "Not Enough Arguments. Try Again\n";
-    //     return (1);
-    // }
-    // ost.open(av[1]);
-    // ost << "TTA TL9 RA RJLI HADIK \n";
-    while (getline(ifst, str))
-        std::cout << str;
-    ost.close();
+    if (ac != 4) {
+        std::cout << "Error: Wrong arguments. Try again\n";
+        return (1);
+    }
+    for (int i = 0; i < 3; i++)
+        data[i] = av[i + 1];
+    ifs.open(data[0]);
+    os.open(data[0].append(".replace"));
+    if (!ifs.is_open() || !os.is_open()) {      
+        std::cout << "Error: Couldn't open the file. Try again\n";
+        return (1);
+    }
+    getline(ifs, str, '\0');
+    pos = 0;
+    while (pos != std::string::npos) {
+        pos = str.find(data[1]);
+        if (pos != std::string::npos) {
+            str.erase(pos, data[1].length());
+            str.insert(pos, data[2]);
+        }
+    }
+    os << str;
     return (0);
 }
