d0973aa19abfe8a3a5bf562d47ede1dffeaa1736
ilahyani <lahyani799@gmail.com>
2022-11-25 10:13:37 +0100

    Desctructor msg printed

--- module03/ScavTrap/ScavTrap.cpp
+++ module03/ScavTrap/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 08:43:20 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/11/25 09:44:43 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/11/25 10:01:58 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,9 @@ ScavTrap::ScavTrap(std::string str) : ClapTrap(str) {
     AttackDamage = 20;
 }
 
-ScavTrap::~ScavTrap() {}
+ScavTrap::~ScavTrap() {
+    std::cout << "ScavTrap Default Destructor Called\n";
+}
 
 void    ScavTrap::guardGate(void) {
     std::cout << "ScavTrap is now in Gatekeeper mode\n";
