5cbd14bdf85b82d426f6dccd0ebd7ce3e16aeed7
ilahyani <lahyani799@gmail.com>
2022-12-04 14:09:31 +0100

    go

---
+++ module07/ex02/Array.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 11:40:05 by ilahyani          #+#    #+#             */
+/*   Updated: 2022/12/04 13:46:26 by ilahyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+template<typename T>
+class Array
+{
+    T *tab;
+    unsigned int len;
+    public:
+        Array() {
+            tab = 0;
+            len = 0;
+        }
+        Array(unsigned int n) : len(n) {
+            tab = new T[n];
+        }
+        Array(Array &copy) {
+            *this = copy;
+        }
+        Array& operator=(Array& other) {
+            len = other.len;
+            tab = new T[other.len];
+            for (unsigned int i = 0; i < len; i++)
+                tab[i] = other.tab[i];
+            return (*this);
+        }
+        T&  operator[](unsigned int idx) {
+            if (idx < 0 || idx > len)
+                throw (OutOfBound());
+            return (tab[idx]);
+        }
+        class OutOfBound: public std::exception {
+            virtual const char* what() const throw() {
+                return ("Element out of bond\n");
+            }
+        };
+        unsigned int size(void) const {
+            return (len);
+        }
+};
+
+#endif
---
+++ module07/ex02/main.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 10:47:11 by ilahyani          #+#    #+#             */
+/*   Updated: 2022/12/04 14:08:59 by ilahyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+#define T char
+
+int main() {
+
+    Array<T> y;
+    Array<T> x(10);
+
+    for (int i = 0; i < 10; i++)
+        x[i] = (i + 65);
+    std::cout << "[Original Array]: ";
+    for (int i = 0; i < 10; i++)
+        std::cout << x[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "[Assignemet Operator Test]: ";
+    Array<T> z = x;
+    for (int i = 0; i < 10; i++)
+        std::cout << z[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "[Copy Constructor Test]: ";
+    Array<T> k(z);
+    for (int i = 0; i < 10; i++)
+        std::cout << k[i] << " ";
+    std::cout << "\n";
+
+    std::cout << "[Deep Copy Test Test]: ";
+    for (int i = 0; i < 10; i++)
+        x[i] = (i + 97);
+    std::cout << "\n";
+    std::cout << "[Original Array]:   ";
+    for (int i = 0; i < 10; i++)
+        std::cout << x[i] << " ";
+    std::cout << "\n[Assigned Array]: ";
+    for (int i = 0; i < 10; i++)
+        std::cout << z[i] << " ";
+    std::cout << "\n[Copied Array]:   ";
+    for (int i = 0; i < 10; i++)
+        std::cout << k[i] << " ";
+    std::cout << "\n";
+
+    try {
+        std::cout << k[1000000] << "\n";
+    }
+    catch (std::exception& e) {
+        std::cout << e.what();
+    }
+    try {
+        std::cout << z[-5] << "\n";
+    }
+    catch (std::exception& e) {
+        std::cout << e.what();
+    }
+    try {
+        std::cout << x[11] << "\n";
+    }
+    catch (std::exception& e) {
+        std::cout << e.what();
+    }
+
+    std::cout << y.size() << "\n";
+    std::cout << x.size() << "\n";
+    std::cout << z.size() << "\n";
+    std::cout << k.size() << "\n";
+    return (0);
+}
