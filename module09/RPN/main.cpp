228682bb1f71e4e0c1bdd49712bc101e0c752a27
ilahyani <ilahyani@student.1337.ma>
2023-03-18 16:30:31 +0100

    empty stack error check

--- module09/RPN/main.cpp
+++ module09/RPN/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 20:41:53 by ilahyani          #+#    #+#             */
-/*   Updated: 2023/03/15 18:11:42 by ilahyani         ###   ########.fr       */
+/*   Updated: 2023/03/17 21:38:57 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,38 @@ int main(int ac, char **av) {
     std::string str;
     int x;
 
-    if (ac == 2) {
-        str = av[1];
-        if (str.size() == 0)
-            return (std::cerr << "Error\n", 1);
-        for (size_t i = 0; i < str.size(); i++) {
-            if (str[i] == '+' || str[i] == '-' || str[i] == '/' || str[i] == '*') {
-                x = s.top();
-                s.pop();
-                if (s.empty())
+    if (ac != 2)
+        return (std::cerr << "Error\n", 1);
+    str = av[1];
+    if (str.size() == 0)
+        return (std::cerr << "Error\n", 1);
+    for (size_t i = 0; i < str.size(); i++) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '/' || str[i] == '*') {
+            if (s.empty())
+                return (std::cerr << "Error: operation can't be done\n", 1);
+            x = s.top();
+            s.pop();
+            if (s.empty())
+                return (std::cerr << "Error: operation can't be done\n", 1);
+            if (str[i] == '+')
+                s.top() += x;
+            else if (str[i] == '*')
+                s.top() *= x;
+            else if (str[i] == '-')
+                s.top() -= x;
+            else {
+                if (x && s.top())
+                    s.top() /= x;
+                else
                     return (std::cerr << "Error: operation can't be done\n", 1);
-                if (str[i] == '+')
-                    s.top() += x;
-                else if (str[i] == '*')
-                    s.top() *= x;
-                else if (str[i] == '-')
-                    s.top() -= x;
-                else {
-                    if (x && s.top())
-                        s.top() /= x;
-                    else
-                        return (std::cerr << "Error: operation can't be done\n", 1);
-                }
-                continue ;
             }
-            else if (str[i] >= '0' && str[i] <= '9')
-                s.push(str[i] - 48);
-            else if (str[i] != ' ')
-                return (std::cerr << "Error\n", 1);
+            continue ;
         }
-        std::cout << s.top() << "\n";
+        else if (str[i] >= '0' && str[i] <= '9')
+            s.push(static_cast<int>(str[i])); // TEST
+        else if (str[i] != ' ')
+            return (std::cerr << "Error\n", 1);
     }
-    else
-        std::cerr << "Error\n";
+    std::cout << s.top() << "\n";
     return 0;
 }
